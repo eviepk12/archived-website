0b9cfe434b1ceb4bb0afde6eb16b39f67572d6ba
Evieepk12 <eviepk12@gmail.com>
2021-12-28 22:21:46 +0700

    updated layout

---
+++ caesar-script/encryption.js
@@ -0,0 +1,54 @@
+let originalInput = document.querySelector("#original");
+let shiftInput = document.querySelector("#shift");
+let encryptedInput = document.querySelector("#encrypted");
+
+let originalMessage = originalInput.value;
+
+originalInput.addEventListener("input", characterEntered, false);
+shiftInput.addEventListener("input", numberEntered, false);
+
+function characterEntered(e) {
+    originalMessage = e.target.value;
+    originalMessage = originalMessage.toLowerCase();
+    originalMessage = originalMessage.replace(/[^a-z]/,'');
+
+    e.target.value = originalMessage;
+
+    startEncryption();
+}
+
+function numberEntered(e) {
+    startEncryption
+}
+
+function startEncryption() {
+    let encryptedMessage = "";
+    let shift = shiftInput.value ? Number(shiftInput.value) : 0;
+    console.log(shift);
+  
+    for (letter of originalMessage) {
+      encryptedMessage += shiftLetters(letter, shift);
+    }
+  
+    console.log(encryptedMessage);
+    encryptedInput.value = encryptedMessage;
+  }
+  startEncryption();
+
+function shiftLetters(letter, shift) {
+    let newLetter = "";
+
+    let letterCode = letter.charCodeAt(0);
+    let newLetterCode = letterCode + (shift % 26);
+
+    if (newLetterCode < 97) {
+        newLetterCode += 26;
+    } 
+    else if (newLetterCode > 122) {
+        newLetterCode -= 26;
+    }
+
+    newLetter=String.fromCharCode(newLetterCode);
+
+    return newLetter;
+}
