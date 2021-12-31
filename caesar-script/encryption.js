43a2c15a175587b9a383447d8734a180a47337ef
Evieepk12 <eviepk12@gmail.com>
2021-12-31 10:16:13 +0700

    added a feature to input only number in input field

--- caesar-script/encryption.js
+++ caesar-script/encryption.js
@@ -52,3 +52,12 @@ function shiftLetters(letter, shift) {
 
     return newLetter;
 }
+
+function onlyNumberKey(evt) {
+          
+    // Only ASCII character in that range allowed
+    var ASCIICode = (evt.which) ? evt.which : evt.keyCode
+    if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57))
+        return false;
+    return true;
+}
