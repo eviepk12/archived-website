805f229461e83174ae1967ad85c1ff5ff98b5197
Evieepk12 <eviepk12@gmail.com>
2022-01-06 23:47:34 +0700

    added more to the converter project

--- converter/onlyNumber.js
+++ converter/onlyNumber.js
@@ -1,3 +1,26 @@
+function decToBinary(n, base) {
+    if (n < 0) {
+        n = 0xFFFFFFFF + n + 1;
+    }
+
+    switch (base) {
+        
+    case 'B':  
+    return parseInt(n, 10).toString(2);
+    break;  
+    case 'H':  
+    return parseInt(n, 10).toString(16);
+    break;  
+    case 'O':  
+    return parseInt(n, 10).toString(8);
+    break;  
+    default:  
+    return("Wrong input.........");
+    }
+}
+
+console.log(decToBinary(5));
+
 function onlyNumberKey(evt) {
           
     // Only ASCII character in that range allowed
